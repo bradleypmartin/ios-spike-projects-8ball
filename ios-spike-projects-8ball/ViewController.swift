9b5bb2cc8ad0648f233054f096c88afc4d3a2468
bradleypmartin <brad.martin@ibotta.com>
2019-02-03 21:08:14 -0700

    Initial Commit

---
+++ ios-spike-projects-8ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-spike-projects-8ball
+//
+//  Created by Brad Martin on 2/3/19.
+//  Copyright © 2019 Brad Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
